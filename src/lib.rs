9e4ce06020693bb87a7a3f8943a76ca414143540
Marcella Hastings <marcellahastings@gmail.com>
2021-04-14 11:56:59 -0400

    define signature and blind signature apis

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
+pub mod blinded_signatures;
+pub mod signatures;
 
+#[cfg(test)]
+mod tests {
+    use crate::signatures::KeyPair;
+
+    #[test]
+    #[should_panic]
+    fn make_keypair() {
+        let _kp = KeyPair::new(3);
+    }
+}

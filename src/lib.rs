256079f09101d70294df8949b5bd1eff97d22ca4
Marcella Hastings <marcellahastings@gmail.com>
2021-04-15 14:01:45 -0400

    add basic PS signing

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,29 @@ pub mod signatures;
 
 #[cfg(test)]
 mod tests {
-    use crate::signatures::KeyPair;
+    use crate::signatures::{KeyPair, Message};
+    use bls12_381 as BLS12;
+    use ff::Field;
 
     #[test]
     fn make_keypair() {
         let mut rng = rand::thread_rng();
         let _kp = KeyPair::new(3, &mut rng);
+    }
+
+    #[test]
+    fn signing_is_correct() {
+        let mut rng = rand::thread_rng();
+        let kp = KeyPair::new(3, &mut rng);
+        let msg = Message::new(vec![
+            BLS12::Scalar::random(&mut rng),
+            BLS12::Scalar::random(&mut rng),
+            BLS12::Scalar::random(&mut rng),
+        ]);
+
+        let sig = kp.try_sign(&mut rng, &msg).unwrap();
+        if !kp.verify(&msg, &sig) {
+            panic!("Signature didn't verify!!");
+        }
     }
 }

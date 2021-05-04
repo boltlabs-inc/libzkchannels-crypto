37da5a77c26fa96ef70b476ac4786fdb374d3246
Marcella Hastings <marcellahastings@gmail.com>
2021-05-04 17:26:01 -0400

    update commitment API to work over generic groups

--- libzkchannels-crypto/src/pedersen_commitments.rs
+++ libzkchannels-crypto/src/pedersen_commitments.rs
@@ -2,46 +2,50 @@
 /// "Non-interactive and information-theoretic secure verifiable secret sharing"
 /// Available at: https://www.cs.cornell.edu/courses/cs754/2001fa/129.PDF
 use crate::types::*;
+use group::Group;
 use rand::CryptoRng;
 use rand_core::RngCore;
 
-/// Commitment object
+/// Represents a Pedersen commitment to a message
 #[derive(Debug, Clone)]
 pub struct Commitment;
 
-/// Commitment randomness (generated during commitment)
 #[derive(Debug, Clone, Copy)]
 pub struct CommitmentRandomness(pub Scalar);
 
 impl CommitmentRandomness {
+    /// Creates suitably random commitment randomness
     pub fn new(_rng: &mut (impl CryptoRng + RngCore)) -> Self {
         todo!();
     }
 }
 
+#[allow(unused)]
 /// Parameters for Pedersen commitments
-pub struct PedersenParameters;
-
-impl PedersenParameters {
-    /// Generates new generators to use in Pedersen commitments
-    pub fn new(_rng: &mut (impl CryptoRng + RngCore)) -> Self {
-        todo!();
-    }
+/// These must be defined over a suitable group.
+/// The group must support scalar multiplication and addition over elements, and generation of random values
+/// In practice, this should only be used with the groups G1Projective and G2Projective from BLS12-381
+pub struct PedersenParameters<T>
+where
+    T: Group<Scalar = Scalar>,
+{
+    h: T,
+    gs: Vec<T>,
 }
 
-impl Message {
-    /// Commits to a message; produces commitment and commitment randomness
-    pub fn commit(&self, _cr: &CommitmentRandomness, _pp: &PedersenParameters) -> Commitment {
+impl<T: Group<Scalar = Scalar>> PedersenParameters<T> {
+    /// Generates random, new parameters for commitments to messages of given length
+    pub fn new(_length: usize, _rng: &mut (impl CryptoRng + RngCore)) -> Self {
+        todo!();
+    }
+
+    /// Commits to a message using the provided commitment randomness
+    pub fn commit(&self, _msg: &Message, _cr: &CommitmentRandomness) -> Commitment {
         todo!();
     }
 
     /// Verifies the commitment on the given message and randomness
-    pub fn decommit(
-        &self,
-        _pp: &PedersenParameters,
-        _com: &Commitment,
-        _r: &CommitmentRandomness,
-    ) -> bool {
+    pub fn decommit(&self, _com: &Commitment, _msg: &Message, _cr: &CommitmentRandomness) -> bool {
         todo!();
     }
 }

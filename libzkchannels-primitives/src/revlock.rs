773317c2f3ac8425dd00824f8bdc66e68229f571
Marcella Hastings <marcellahastings@gmail.com>
2021-05-06 15:13:27 -0400

    add more types for Pay

--- libzkchannels-primitives/src/revlock.rs
+++ libzkchannels-primitives/src/revlock.rs
@@ -6,21 +6,39 @@ pub struct RevocationLock;
 
 pub struct RevocationSecret;
 
-pub fn generate_revocation_pair(
-    _rng: &mut (impl CryptoRng + RngCore),
-) -> (RevocationLock, RevocationSecret) {
-    todo!();
-}
-
 pub struct RevocationLockCommitment(Commitment);
 
+pub struct RevocationLockCommitmentRandomness();
+
 impl RevocationLock {
+    /// Generates a new revocation lock and its corresponding secret
+    pub fn generate_pair(_rng: &mut (impl CryptoRng + RngCore)) -> (Self, RevocationSecret) {
+        todo!();
+    }
+
+    /// Validates a revocation pair
+    pub fn verify(&self, _rs: &RevocationSecret) -> bool {
+        todo!();
+    }
+
     /// Forms a commitment (and corresponding commitment randomness) to a RevocationLock
     pub fn commit(
         &self,
         _rng: &mut (impl CryptoRng + RngCore),
         _param: &CustomerParameters,
-    ) -> (RevocationLockCommitment, CommitmentRandomness) {
+    ) -> (RevocationLockCommitment, RevocationLockCommitmentRandomness) {
+        todo!();
+    }
+}
+
+impl RevocationLockCommitment {
+    /// validates commitment to revocation lock
+    pub fn decommit(
+        &self,
+        _param: &MerchantParameters,
+        _rl: &RevocationLock,
+        _bf: &RevocationLockCommitmentRandomness,
+    ) -> bool {
         todo!();
     }
 }

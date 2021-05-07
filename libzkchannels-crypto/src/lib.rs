7f189d5220c35fed44ab8869c8e5f3c3b2bae310
swan <swan@travellingcryptographer.com>
2021-05-06 21:28:06 -0700

    add some warning lines

--- libzkchannels-crypto/src/lib.rs
+++ libzkchannels-crypto/src/lib.rs
@@ -1,3 +1,9 @@
+#![warn(missing_docs)]
+#![warn(missing_copy_implementations, missing_debug_implementations)]
+#![warn(unused_qualifications, unused_results)]
+#![warn(future_incompatible)]
+#![warn(unused)]
+#![forbid(broken_intra_doc_links)]
 pub mod message;
 pub mod pedersen_commitments;
 pub mod ps_blind_signatures;

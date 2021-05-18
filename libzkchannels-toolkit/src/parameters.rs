86c86b4264393556ebcaf5f7e4361f9e8c102fb6
Marcella Hastings <marcellahastings@gmail.com>
2021-05-18 15:17:48 -0400

    add api for parameter generation

--- libzkchannels-toolkit/src/parameters.rs
+++ libzkchannels-toolkit/src/parameters.rs
@@ -7,18 +7,40 @@ use ps_keys::*;
 #[derive(Debug)]
 /// Keys and parameters used by the customer throughout the lifetime of a zkChannel.
 pub struct CustomerParameters {
+    /*
     /// Merchant public key for signature validation.
     pub merchant_signing_pk: PublicKey,
     /// Parameters for forming general commitments.
     pub commitment_params: PedersenParameters<G1Projective>,
+    */
 }
 
 #[allow(unused)]
 #[derive(Debug)]
 /// Keys and parameters used by the merchant throughout its lifetime (and across all its channels).
 pub struct MerchantParameters {
+    /*
     /// Keypair suitable for signing transactions.
     pub signing_keys: KeyPair,
     /// Parameters for forming general commitments.
     pub commitment_params: PedersenParameters<G1Projective>,
+    */
+}
+
+impl MerchantParameters {
+    /// Generates new merchant parameters.
+    pub fn new(rng: &mut impl Rng) -> Self {
+        MerchantParameters {
+            //signing_keys: KeyPair::new(0, rng),
+            //commitment_params: PedersenParameters::new(0, rng),
+        }
+    }
+
+    /// Extracts public keys from the merchant parameters to produce [`CustomerParameters`]
+    pub fn extract_customer_parameters(&self) -> CustomerParameters {
+        CustomerParameters {
+            //merchant_signing_pk: self.signing_keys.pk.clone(),
+            //// commitment_params: self.commitment_params.clone(),
+        }
+    }
 }

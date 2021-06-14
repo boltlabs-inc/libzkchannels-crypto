1b87d37e5e5d45ed2f7afcb34dab369ef26653d2
Mukund Lakshman <yaymukund@gmail.com>
2021-06-14 14:49:53 +0100

    Condense into a single macro invocation & use fully qualified paths.

--- zkabacus-crypto/src/sqlite.rs
+++ zkabacus-crypto/src/sqlite.rs
@@ -2,54 +2,50 @@ use crate::revlock::{RevocationLock, RevocationSecret};
 use crate::Nonce;
 use std::convert::TryInto;
 
-macro_rules! impl_type_for_scalar_newtype {
-    ($type:ty) => {
-        impl sqlx::Type<::sqlx::Sqlite> for $type {
-            fn type_info() -> sqlx::sqlite::SqliteTypeInfo {
-                <Vec<u8> as sqlx::Type<sqlx::Sqlite>>::type_info()
+macro_rules! impl_sqlx_for_scalar_newtype {
+    ($type:ty, $constructor:ident) => {
+        impl ::sqlx::Type<::sqlx::Sqlite> for $type {
+            fn type_info() -> ::sqlx::sqlite::SqliteTypeInfo {
+                <::std::vec::Vec<::std::primitive::u8> as ::sqlx::Type<::sqlx::Sqlite>>::type_info()
             }
         }
-    };
-}
 
-macro_rules! impl_encode_for_scalar_newtype {
-    ($type:ty) => {
-        impl sqlx::Encode<'_, sqlx::Sqlite> for $type {
+        impl ::sqlx::Encode<'_, ::sqlx::Sqlite> for $type {
             fn encode_by_ref(
                 &self,
-                buf: &mut <sqlx::Sqlite as sqlx::database::HasArguments<'_>>::ArgumentBuffer,
-            ) -> sqlx::encode::IsNull {
+                buf: &mut <::sqlx::Sqlite as ::sqlx::database::HasArguments<'_>>::ArgumentBuffer,
+            ) -> ::sqlx::encode::IsNull {
                 let bytes = self.to_scalar().to_bytes().to_vec();
-                <Vec<u8> as sqlx::Encode<'_, sqlx::Sqlite>>::encode_by_ref(&bytes, buf)
+                <::std::vec::Vec<::std::primitive::u8> as ::sqlx::Encode<'_, ::sqlx::Sqlite>>::encode_by_ref(&bytes, buf)
             }
 
             fn encode(
                 self,
-                buf: &mut <sqlx::Sqlite as sqlx::database::HasArguments<'_>>::ArgumentBuffer,
+                buf: &mut <::sqlx::Sqlite as ::sqlx::database::HasArguments<'_>>::ArgumentBuffer,
             ) -> ::sqlx::encode::IsNull {
                 let bytes = self.to_scalar().to_bytes().to_vec();
-                <Vec<u8> as sqlx::Encode<'_, sqlx::Sqlite>>::encode(bytes, buf)
+                <::std::vec::Vec<::std::primitive::u8> as ::sqlx::Encode<'_, ::sqlx::Sqlite>>::encode(bytes, buf)
             }
         }
-    };
-}
 
-macro_rules! impl_decode_for_scalar_newtype {
-    ($type:ty, $constructor:ident) => {
-        impl sqlx::Decode<'_, sqlx::Sqlite> for $type {
+        impl ::sqlx::Decode<'_, ::sqlx::Sqlite> for $type {
             fn decode(
-                value: sqlx::sqlite::SqliteValueRef<'_>,
-            ) -> Result<Self, ::sqlx::error::BoxDynError> {
-                if sqlx::ValueRef::is_null(&value) {
-                    return Err(Box::new(sqlx::error::UnexpectedNullError));
+                value: ::sqlx::sqlite::SqliteValueRef<'_>,
+            ) -> ::std::result::Result<Self, ::sqlx::error::BoxDynError> {
+                if ::sqlx::ValueRef::is_null(&value) {
+                    return ::std::result::Result::Err(Box::new(
+                        ::sqlx::error::UnexpectedNullError,
+                    ));
                 }
 
-                let blob = <&[u8] as sqlx::Decode<sqlx::Sqlite>>::decode(value)?.try_into()?;
-                let maybe_scalar: Option<$crate::types::Scalar> =
+                let blob =
+                    <&[::std::primitive::u8] as ::sqlx::Decode<::sqlx::Sqlite>>::decode(value)?
+                        .try_into()?;
+                let maybe_scalar: ::std::option::Option<$crate::types::Scalar> =
                     $crate::types::Scalar::from_bytes(blob).into();
 
                 let res = maybe_scalar.map($constructor).ok_or_else(|| {
-                    sqlx::Error::Decode(
+                    ::sqlx::Error::Decode(
                         String::from("tried to decode a nonce from non-canonical byte input")
                             .into(),
                     )
@@ -61,13 +57,6 @@ macro_rules! impl_decode_for_scalar_newtype {
     };
 }
 
-impl_encode_for_scalar_newtype!(Nonce);
-impl_type_for_scalar_newtype!(Nonce);
-
-impl_encode_for_scalar_newtype!(RevocationLock);
-impl_decode_for_scalar_newtype!(RevocationLock, RevocationLock);
-impl_type_for_scalar_newtype!(RevocationLock);
-
-impl_encode_for_scalar_newtype!(RevocationSecret);
-impl_decode_for_scalar_newtype!(RevocationSecret, RevocationSecret);
-impl_type_for_scalar_newtype!(RevocationSecret);
+impl_sqlx_for_scalar_newtype!(Nonce, Nonce);
+impl_sqlx_for_scalar_newtype!(RevocationLock, RevocationLock);
+impl_sqlx_for_scalar_newtype!(RevocationSecret, RevocationSecret);

b52c55935b04cab804276432e255038833f6f843
Marcella Hastings <marcellahastings@gmail.com>
2021-07-30 17:15:43 -0400

    add docs about proof constraints #25 #54

--- zkchannels-crypto/src/proofs.rs
+++ zkchannels-crypto/src/proofs.rs
@@ -112,23 +112,94 @@
 //! The library includes integrated support for range constraints and allows manual addition and
 //! verification of other types of constraints.
 //!
+//! The library supports combining multiple proof objects and constraints. But this combination
+//! requires some expertise to do correctly!
+//! Most constraints involve manipulation of the challenge scalars. Applying relationships to
+//! the challenge scalars can enforce properties on the underlying messages, and will lead to
+//! equivalent relationships in the response scalars. To validate the proof, check that these
+//! relationships still hold with respect to the response scalars.
+//!
+//! To specify
+//! To access commitment scalars, use the `conjunction_commitment_scalars()` function on the
+//! `ProofBuilder`.
+//! To access response scalars, use the `conjunction_response_scalars()` function on the `Proof`.
+//! Each of these functions return a list; the `j`th element corresponds to the `j`th message in
+//! the message tuple.
+//!
+//! Some general guidelines follow:
+//!
+//! 1. All components of the proof must be generated in parallel. That is, you must run the
+//!    commitment phase for all proof objects and constraints before generating a challenge.
+//!    You must use the same challenge to execute each response phase.
+//!
+//! 2. The Challenge phase must generate a single challenge that incorporates _all_ proof
+//!    components.
+//!
+//! 3. All constraints that are manually added must also be manually validated. Calling the
+//!    built-in validation functions _is not enough_.
+//!
+//! The following sections will give details on how to enforce specific types of constraints.
+//! For complete code, see the examples in this crate.
 //!
 //!
 //!
+//! ## Partial openings
+//! A partial opening proves that one message in the message tuple matches a known, public
+//! value.
+//!
+//! Modifications to standard flow:
+//! 1. **Challenge phase**. Include the proof and the commitment scalar corresponding to the public
+//!   value in the challenge.
+//! 2. **Final proof**. Include the public value and its commitment scalar along with the proof.
+//! 3. **Verification**. Check the following equality for each opened value:
+//!    ```
+//!    challenge * public_value + public_value_commitment_scalar == public_value_response_scalar
+//!    ```
+//!    Also check that the public value in the proof statement matches the expected value.
 //!
 //!
+//! ## Equality checks
+//! An equality check can enforce that two messages in a tuple have the same value.
+//!
+//! Modifications to standard flow:
+//! 1. **Commitment phase**. The two messages must have the same commitment scalar. If they are in
+//!    the same proof, generate a new commitment scalar (uniformly at random) and pass it to the 
+//!    commitment phase via the `conjunction_commitment_scalars` argument. 
+//!
+//!    If they are in different proofs, generate the first proof normally, then retrieve
+//!    the commitment scalar corresponding to the message and pass it to the second proof via the 
+//!    `conjunction_commitment_scalars` argument. 
+//!
+//! 2. **Verification**. Check that the response scalars for each matching value are equal.
 //!
 //!
+//! ## Linear combinations
+//! A linear combination enforces that a message is either
+//! the sum of two other messages or
+//! the sum of a message and a public value (e.g. that is not contained in the proof).
+//!
+//! To enforce the relationship `m1 + m2 = m3`, make these modifications:
+//! 1. **Commitment phase**. The commitment scalar for `m3` must equal the sum of the commitment
+//!    scalars for `m1` and `m2`.
+//! 
+//! 2. **Verification**. The response scalar for `m3` must equal the sum of the response scalars
+//!    for `m1` and `m2`.
+//!
+//! To enforce the relationship `m1 + public = m2`, make these modifications:
+//! 1. **Commitment phase**. The commitment scalar for `m2` must equal that of `m1`.
+//! 
+//! 2. **Challenge phase**. Include the public value in the challenge.
+//! 
+//! 2. **Final proof**. Include the public value along with the proof.
+//!
+//! 3. **Verification**. Let `r1` and `r2` be the response scalars corresponding to `m1` and `m2`:
+//!    ```
+//!    r2 == r1 + challenge * public
+//!    ```
+//!    Also check that the public value in the proof statement matches the expected value.
 //!
 //!
-//!
-//!
-//!
-//!
-//!
-//!
-//!
-//!
+//! ## Range proofs
 //!
 //!
 //!
